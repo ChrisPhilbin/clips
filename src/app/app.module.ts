db7ebc083ce2ea7f68cc8e8c956331daf181708d
Chris Philbin <chrisp@runway.com.au>
2022-05-22 18:56:18 -0400

    Working on file uploads and implementing uuid to generate a random/unique filename

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AboutComponent } from './about/about.component';
 import { VideoModule } from './video/video.module';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './notfound/notfound.component';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { NotFoundComponent } from './notfound/notfound.component';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
+    AngularFireStorageModule,
     VideoModule,
     AppRoutingModule,
   ],
--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-upload',
@@ -10,6 +12,10 @@ export class UploadComponent implements OnInit {
   isDragover = false;
   file: File | null = null;
   nextStep = false;
+  showAlert = false;
+  alertColor = 'blue';
+  alertMsg = 'Please wait! Your clip is being uploaded.';
+  inSubmission = false;
 
   title = new FormControl('', [Validators.required, Validators.minLength(3)]);
 
@@ -17,22 +23,29 @@ export class UploadComponent implements OnInit {
     title: this.title,
   });
 
-  constructor() {}
+  constructor(private storage: AngularFireStorage) {}
 
   ngOnInit(): void {}
 
   storeFile($event: Event) {
     this.isDragover = false;
     this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
-    if (!this.file) {
+    if (!this.file || this.file.type !== 'video/mp4') {
       return;
     }
     this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
     this.nextStep = true;
-    console.log(this.file);
   }
 
   uploadFile() {
-    console.log('File uploaded.');
+    this.showAlert = true;
+    this.alertColor = 'blue';
+    this.alertMsg = 'Please wait! Your clip is being uploaded.';
+    this.inSubmission = true;
+
+    const clipFileName = uuid();
+    const clipPath = `clips/${clipFileName}.mp4`;
+
+    this.storage.upload(clipPath, this.file);
   }
 }
