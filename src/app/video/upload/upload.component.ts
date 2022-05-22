778bbf6b745a06690bdb3309e1fa5c5063cf3dc4
Chris Philbin <chrisp@runway.com.au>
2022-05-22 16:21:58 -0400

    Creating custom directive for dragging events and beginning work on upload form component

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
 })
 export class UploadComponent implements OnInit {
+  isDragover = false;
+  file: File | null = null;
+  nextStep = false;
 
-  constructor() { }
+  title = new FormControl('', [Validators.required, Validators.minLength(3)]);
 
-  ngOnInit(): void {
+  uploadForm = new FormGroup({
+    title: this.title,
+  });
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  storeFile($event: Event) {
+    this.isDragover = false;
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+    if (!this.file) {
+      return;
+    }
+    this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
+    this.nextStep = true;
+    console.log(this.file);
   }
 
+  uploadFile() {
+    console.log('File uploaded.');
+  }
 }
