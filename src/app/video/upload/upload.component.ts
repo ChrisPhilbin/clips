9664068fcfa2a217cce6b7426c5c044112eec4d3
Chris Philbin <chrisp@runway.com.au>
2022-06-02 17:32:10 -0400

    Handling multiple screenshots from a single video upload

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -51,7 +51,7 @@ export class UploadComponent implements OnDestroy {
     this.task?.cancel();
   }
 
-  storeFile($event: Event) {
+  async storeFile($event: Event) {
     this.isDragover = false;
     this.file = ($event as DragEvent).dataTransfer
       ? ($event as DragEvent).dataTransfer?.files.item(0) ?? null
@@ -59,6 +59,9 @@ export class UploadComponent implements OnDestroy {
     if (!this.file || this.file.type !== 'video/mp4') {
       return;
     }
+
+    await this.ffmpegService.getScreenshots(this.file);
+
     this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
     this.nextStep = true;
   }
