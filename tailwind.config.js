5086d627b32dcbd1adb097172c085b3c9c665a9d
Chris Philbin <chrisp@runway.com.au>
2022-07-17 07:54:36 -0400

    Updating styles and fixing broken image thumbnails

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
+// module.exports = {
+//   content: ["./src/**/*.{html,ts}"],
+//   safelist: ["bg-blue-400", "bg-green-400", "bg-red-400"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ["./src/app/**/*.{html,js}"],
   safelist: ["bg-blue-400", "bg-green-400", "bg-red-400"],
   theme: {
     extend: {},
